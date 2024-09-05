c75b11fd39324bb8d851f2823ce1d6ef303c3e1c
TamajiNeko <classroom3.5.17@gmail.com>
2024-09-05 18:10:31 +0700

    v0.5144t3(js)

--- script/export.js
+++ script/export.js
@@ -1,23 +1,18 @@
 document.getElementById("export-button").addEventListener("click", function () {
     var cardElement = document.getElementById("cardContainer");
-
-    // Set scaling using percentage values as you prefer
+    
     cardElement.style.transform = "scale(80%)";
-
-    // Capture the card container with transparent background
     html2canvas(cardElement, {
-        backgroundColor: null,  // Set the background to transparent
-        //useCORS: true           // Enable cross-origin resource sharing if needed
+        backgroundColor: null,
+        useCORS: true
     }).then(function (canvas) {
         var image = canvas.toDataURL("image/png");
 
-        // Create a link to download the image
         var link = document.createElement("a");
         link.href = image;
         link.download = "card-image.png";
         link.click();
-
-        // Reset the scale after export
+        
         cardElement.style.transform = "scale(15%)";
     });
 });
