41f24ae43e309f55d9d152c0f8dc931c79d3eb4e
TamajiNeko <classroom3.5.17@gmail.com>
2024-09-05 19:30:32 +0700

    v0.1548 (js)

--- Script/export.js
+++ Script/export.js
@@ -1,17 +1,17 @@
 document.getElementById("export-button").addEventListener("click", function () {
     var cardElement = document.getElementById("cardContainer");
 
-    cardElement.style.scale = "80%";
+    cardElement.style.scale = "100%";
     html2canvas(cardElement, {
-        backgroundColor: null,
+    backgroundColor: null,
     }).then(function (canvas) {
-        var image = canvas.toDataURL("image/png");
+    var image = canvas.toDataURL("image/png");
 
-        var link = document.createElement("a");
-        link.href = image;
-        link.download = "card-image.png";
-        link.click();
-        
-        cardElement.style.scale = "15%";
+    var link = document.createElement("a");
+    link.href = image;
+    link.download = "card-image.png";
+    link.click();
+    
+    cardElement.style.scale = "15%";
     });
 });
